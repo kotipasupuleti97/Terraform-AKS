fba4fa8e3729ec6abe80c17c85b8aaee1cffc138
koti pasupuleti <kotipasupuleti97@gmail.com>
2024-11-07 16:59:33 +0530

    remove defaults

--- variables.tf
+++ variables.tf
@@ -1,55 +1,55 @@
 variable "location" {
   description = "The Azure region where the resources will be created."
   type        = string
-  default = "CentralIndia"
+  # default = "CentralIndia"
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group for the AKS cluster."
   type        = string
-  default = "aks_ckuster_rg"
+  # default = "aks_ckuster_rg"
 }
 
 variable "cluster_name" {
   description = "Name of the AKS cluster."
   type        = string
-  default = "aks-cluster"
+  # default = "aks-cluster"
 }
 
 variable "dns_prefix" {
   description = "DNS prefix for the AKS cluster."
   type        = string
-  default = "akscluster"
+  # default = "akscluster"
 }
 
 variable "default_node_count" {
   description = "Number of nodes in the default node pool."
   type        = number
-  default     = 1
+  # default     = 1
 }
 
 variable "default_vm_size" {
   description = "VM size for the default node pool."
   type        = string
-  default     = "Standard_D2s_v3"
+  # default     = "Standard_D2s_v3"
 }
 
 variable "auto_scaling_enabled" {
   description = "Whether to enable auto-scaling for the default node pool."
   type        = bool
-  default     = true
+  # default     = true
 }
 
 variable "default_min_count" {
   description = "Minimum node count for the default node pool when auto-scaling is enabled."
   type        = number
-  default     = null
+  # default     = null
 }
 
 variable "default_max_count" {
   description = "Maximum node count for the default node pool when auto-scaling is enabled."
   type        = number
-  default     = null
+  # default     = null
 }
 
 
@@ -57,8 +57,8 @@ variable "default_max_count" {
 variable "tags" {
   description = "Tags to apply to the resources."
   type        = map(string)
-  default = {
-    "env" = "production"
-  }
+  # default = {
+  #   "env" = "production"
+  # }
 }
 
